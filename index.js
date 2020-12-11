444ce3046584c9c0fe1f0031443c5cf542b2592f
Abelardjohn <abelardpangalila@gmail.com>
2020-12-11 14:46:07 +0700

    final project wd

---
+++ index.js
@@ -0,0 +1,44 @@
+function abuabu(){
+    
+    document.body.style.backgroundColor="lightgray"
+    document.body.style.color="black"
+    document.querySelector('.awards').style.backgroundColor="grey";
+    document.querySelector('.awards').style.color="black";
+    document.querySelector('.hobby').style.backgroundColor="grey";
+    document.querySelector('.hobby').style.color="black";
+    document.querySelector('.skills').style.backgroundColor="grey";
+    document.querySelector('.skills').style.color="black";
+    document.querySelector('.contact').style.backgroundColor="grey";
+    document.querySelector('.contact').style.color="black";
+    document.querySelector('.education').style.backgroundColor="grey";
+    document.querySelector('.education').style.color="black";  
+}
+function putih(){
+    document.body.style.backgroundColor="lightgray";
+    document.body.style.color="black";
+    document.querySelector('.awards').style.backgroundColor="white";
+    document.querySelector('.awards').style.color="black";
+    document.querySelector('.hobby').style.backgroundColor="white";
+    document.querySelector('.hobby').style.color="black";
+    document.querySelector('.skills').style.backgroundColor="white";
+    document.querySelector('.skills').style.color="black";
+    document.querySelector('.contact').style.backgroundColor="white";
+    document.querySelector('.contact').style.color="black";
+    document.querySelector('.education').style.backgroundColor="white";
+    document.querySelector('.education').style.color="black";
+}
+function namee(){
+    let nama = prompt("Masukkan Nama");
+    console.log("Nama anda adalah" + namee);
+}
+function email(){
+    let email = prompt("Masukkan Email");
+    console.log("Email = " + email);
+}
+function massage(){
+    let massage = prompt("Tinggalkan Deskripsi");
+    console.log("Deskripsi = " + massage);
+}
+function submit(){
+    alert("Data anda tersimpan");
+}
